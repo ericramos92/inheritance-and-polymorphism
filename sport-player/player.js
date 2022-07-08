8f6e3850460578b2b9cbf3e0fcae5c4e3b772b57
EricRamos <ericramosgarnica1@gmail.com>
2022-07-07 17:26:16 -0700

    player

--- sport-player/player.js
+++ sport-player/player.js
@@ -1,1 +1,26 @@
-//hi
+class Player{
+    constructor(name, age, height, weight){ 
+        this._name = name;
+        this._age = age;
+        this._height = height;
+        this._weight = weight;
+    }
+    get name(){
+        return this._name;
+    }
+    get age(){
+        return  `${this._name} is age ${this._age}`;
+    }
+    get height(){
+        return `${this._name} is ${this._height}cm`;
+    }
+    get weight(){
+        return `${this._name} weights ${this._weight}`;
+    }
+}
+
+const p = new Player('David',25,175,75);
+
+console.log(p.age)
+console.log(p.height)
+console.log(p.weight)
